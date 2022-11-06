47c822b3d4264df9ac8c5117e7dbbf433eda8f78
Alex <71722762+alexespinoza007@users.noreply.github.com>
2022-11-06 14:54:07 -0500

    Add files via upload

---
+++ Car Diagnostic StoryBoard/11_06_2022/CarDiagMainPage/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LoginViewController.swift
+//  CarDiagMainPage
+//
+//  Created by Alex Espinoza on 11/1/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+        // Do any additional setup after loading the view.
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        validateFields()
+    }
+    
+    @IBAction func createAccountTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "signUp")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    func validateFields(){
+        if email.text?.isEmpty == true{
+            print("No Email Text")
+            return
+        }
+        
+        if password.text?.isEmpty == true{
+            print("No Password")
+            return
+        }
+        
+        login()
+    }
+    
+    func login(){
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { [weak self] authResult, err in
+            guard let strongSelf = self else {return}
+            if let err = err {
+                print(err.localizedDescription)
+            }
+            self!.checkUserInfo()
+        }
+    }
+    
+    func checkUserInfo() {
+        if Auth.auth().currentUser != nil{
+            print(Auth.auth().currentUser?.uid)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "mainHome")
+            vc.modalPresentationStyle = .overFullScreen
+            present(vc, animated: true)
+        }
+    }
+}
